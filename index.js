efeee0844612ba6469ba4b2b267a6cd911af551a
Luis Irias <luis.irias@aol.com>
2021-04-01 20:11:40 -0600

    Deployment

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ app.use(cors());
 app.use(express.json({ extended: true }))
 
 //Puerto de la app.
-const port = process.env.port || 4000; //Cualquier puerto excepto el 3000 que es el del cliente.
+const PORT = process.env.PORT || 4000; //Cualquier puerto excepto el 3000 que es el del cliente.
 
-//Importar rutas
+//ImPORTar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 //Arrancando la app o server.
-app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor esta funcionando en el puerto ${port}`);
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
